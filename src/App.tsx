ecda43b0f55b1e0b2a244121d0c03024a9e9d94e
alvitazabaite <162551294+alvitazabaite@users.noreply.github.com>
2025-04-10 00:02:03 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import Header from './components/Header/Header.tsx';
+import viteLogo from '/vite.svg';
+import reactLogo from '../../assets/react.svg';
 
-function App() {
+export default function Header() {
   return (
-    <>
-      <Header />
-      <div className="text-red-500 pt-20">Vite + React</div>
-    </>
+    <header className="bg-amber-400 h-[60px] w-full fixed">
+      <div className="flex justify-center h-full">
+        <img src={viteLogo} className="animate-spin-reverse-slow" alt="Vite logo" />
+        <img src={reactLogo} className="animate-spin-reverse-slower" alt="React logo" />
+      </div>
+    </header>
   );
 }
-
-export default App;
