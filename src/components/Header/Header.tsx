fb9537e16f02ad48c2cecd60eee0997da0866bc5
alvitazabaite <162551294+alvitazabaite@users.noreply.github.com>
2025-04-10 00:02:53 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <header className="bg-amber-400 h-[60px] w-full fixed">
       <div className="flex justify-center h-full">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={viteLogo} className="animate-spin-reverse-slow" alt="Vite logo" />
+        <img src={reactLogo} className="animate-spin-reverse-slower" alt="React logo" />
       </div>
     </header>
   );
