7bf63e85ca0acee82757c313c11c773430035dfd
Alvita Žabaite <alvita.zabaite@surfsharkteam.com>
2025-04-15 20:02:00 +0300

    MR-1: updates of lint

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,20 @@
-import viteLogo from '/vite.svg';
-import reactLogo from '../../assets/react.svg';
+import viteLogo from "/vite.svg";
+import reactLogo from "../../assets/react.svg";
 
 export default function Header() {
   return (
     <header className="bg-amber-400 h-[60px] w-full fixed">
       <div className="flex justify-center h-full">
-        <img src={viteLogo} className="animate-spin-reverse-slow" alt="Vite logo" />
-        <img src={reactLogo} className="animate-spin-reverse-slower" alt="React logo" />
+        <img
+          src={viteLogo}
+          className="animate-spin-reverse-slow"
+          alt="Vite logo"
+        />
+        <img
+          src={reactLogo}
+          className="animate-spin-reverse-slower"
+          alt="React logo"
+        />
       </div>
     </header>
   );
