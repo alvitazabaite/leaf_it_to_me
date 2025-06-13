b3689e64e75b50b10affbde52def9a0ec1745648
Alvita Žabaite <alvita.zabaite@surfsharkteam.com>
2025-06-13 08:59:01 +0300

    MR-5: fix - deleting token

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://yrunhiglbpbmuynkurtq.supabase.co';
-const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlydW5oaWdsYnBibXV5bmt1cnRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxMjAwMjMsImV4cCI6MjA2NDY5NjAyM30.UZh9JGP_fQIFeqDYBL56OLGhYLt213DHllvNwIorg20';
-
+const supabaseKey = '';
 export const supabase = createClient(supabaseUrl, supabaseKey);
