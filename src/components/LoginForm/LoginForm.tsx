ffccacf12bd72ff0b93f33f35da531390daa14a0
Alvita Žabaite <alvita.zabaite@surfsharkteam.com>
2025-09-14 19:19:39 +0300

    MR-6: register and login form updates - toggle

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -5,6 +5,8 @@ import { Button } from '@/components/ui/button.tsx';
 import { clsx } from 'clsx';
 import { useAuth } from '@/context/AuthContext.ts';
 import { toast } from 'sonner';
+import { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
 
 export function LoginForm() {
     const { login } = useAuth();
@@ -13,6 +15,8 @@ export function LoginForm() {
         handleSubmit,
         formState: { isValid },
     } = useForm<LoginFormInput>();
+
+    const [showPassword, setShowPassword] = useState(false);
 
     const onSubmit: SubmitHandler<LoginFormInput> = async data => {
         const error = await login(data);
@@ -26,28 +30,53 @@ export function LoginForm() {
     };
 
     return (
-        <form className="flex flex-col gap-4 mb-2 font-bold" onSubmit={handleSubmit(onSubmit)}>
-            <Input
-                className="text-sm md:text-base w-[250px] md:w-[350px] bg-white"
-                placeholder={'Email'}
-                {...register('email', {
-                    required: true,
-                    pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                })}
-            />
-            <Input
-                className="text-sm md:text-base w-[250px] md:w-[350px] bg-white"
-                type={'password'}
-                placeholder={'Password'}
-                {...register('password', { required: true })}
-            />
+        <form className="flex flex-col gap-6 mb-2" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col">
+                <label className="block text-light-sand text-sm" htmlFor="email">
+                    Email
+                </label>
+                <Input
+                    id="email"
+                    className="text-sm md:text-base w-[250px] md:w-[350px] bg-white"
+                    placeholder={'Enter email address'}
+                    {...register('email', {
+                        required: true,
+                        pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    })}
+                />
+            </div>
+            <div className="flex flex-col">
+                <label className="block text-light-sand text-sm" htmlFor="password">
+                    Password
+                </label>
+                <div className="relative w-[250px] md:w-[350px]">
+                    <Input
+                        id="password"
+                        className="text-sm md:text-base bg-white"
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder={'Enter password'}
+                        {...register('password', { required: true })}
+                    />
+                    <button
+                        type="button"
+                        onClick={() => setShowPassword(prev => !prev)}
+                        className="absolute top-3 md:top-2 right-2 text-gray-500 hover:text-gray-700"
+                    >
+                        {showPassword ? (
+                            <EyeOff className="w-3 h-3 md:w-5 md:h-5" />
+                        ) : (
+                            <Eye className="w-3 h-3 md:w-5 md:h-5" />
+                        )}
+                    </button>
+                </div>
+            </div>
             <div className="flex justify-center">
                 <Button
                     className={clsx(
                         'text-base md:text-lg font-bold',
                         isValid
-                            ? 'cursor-pointer text-white bg-dark-gray-3 hover:bg-dark-gray-2'
-                            : 'cursor-not-allowed text-black bg-dark-gray-1',
+                            ? 'cursor-pointer text-dark-green bg-light-sand hover:bg-dark-gray-2'
+                            : 'cursor-not-allowed text-dark-green bg-dark-gray-1',
                     )}
                     disabled={!isValid}
                     type="submit"
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,11 +2,11 @@ import { LoginForm } from '@/components/LoginForm/LoginForm.tsx';
 
 export default function Login() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-dark-green font-bold">
+        <div className="min-h-screen flex flex-col items-center justify-center bg-dark-green">
             <LoginForm />
-            <div className="text-sm md:text-base">
+            <div className="text-sm md:text-base font-bold text-light-sand">
                 Don’t have an account?{' '}
-                <a href="/register" className="text-blue-500 hover:underline">
+                <a href="/register" className="text-blue-300 hover:underline">
                     Register
                 </a>
             </div>
